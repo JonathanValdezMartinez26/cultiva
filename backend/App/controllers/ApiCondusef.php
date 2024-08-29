2e547e8b8131eed69d3cd2bda60d1a493fde7b87
soporteMCM <alberto.s@masconmenos.com.mx>
2024-08-29 15:55:11 -0600

    ajuste catalogos

--- backend/App/controllers/ApiCondusef.php
+++ backend/App/controllers/ApiCondusef.php
@@ -313,6 +313,7 @@ class ApiCondusef extends Controller
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($ch);
 
         if (curl_errno($ch)) return [];
@@ -327,6 +328,7 @@ class ApiCondusef extends Controller
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         $response = curl_exec($ch);
 
         if (curl_errno($ch)) return [];
