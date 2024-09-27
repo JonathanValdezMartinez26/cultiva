26e8c8c0c4183786bda7ebf7825fd461fed780a2
soporteMCM <alberto.s@masconmenos.com.mx>
2024-09-27 12:06:13 -0600

    Comentarios en clase para generación de archivos xlsx

--- backend/App/libs/PhpSpreadsheet/PhpSpreadsheet.php
+++ backend/App/libs/PhpSpreadsheet/PhpSpreadsheet.php
@@ -8,6 +8,19 @@ use PhpOffice\PhpSpreadsheet\Style;
 
 class PHPSpreadsheet
 {
+    /**
+     * ColumnaExcel
+     * 
+     * Genera una configuración de columna para Excel.
+     *
+     * @param string $letra La letra de la columna en Excel.
+     * @param string $campo El nombre del campo asociado a la columna.
+     * @param string $titulo (Opcional) El título de la columna. Si no se proporciona, se usará el nombre del campo.
+     * @param array $estilo (Opcional) Un array con los estilos aplicables a la columna.
+     * @param bool $total (Opcional) Indica si la columna es un total. Por defecto es false.
+     *
+     * @return array Un array con la configuración de la columna, incluyendo la letra, el campo, el estilo, el título y si es un total.
+     */
     public static function ColumnaExcel($letra, $campo, $titulo = '', $estilo = [], $total = false)
     {
         $titulo = $titulo == '' ? $campo : $titulo;
@@ -22,6 +35,8 @@ class PHPSpreadsheet
     }
 
     /**
+     * GetEstilosExcel
+     * 
      * Este método devuelve un array de estilos predefinidos para hojas de Excel.
      * Los estilos incluyen:
      * - 'titulo': Fuente en negrita, alineación centrada y bordes delgados alrededor de todas las celdas.
@@ -29,6 +44,7 @@ class PHPSpreadsheet
      * - 'moneda': Alineación a la derecha con un formato de número de moneda simple ($1,000.00).
      * - 'fecha': Alineación centrada con un formato de fecha (DD/MM/YYYY).
      * - 'fecha_hora': Alineación centrada con un formato de fecha y hora (DD/MM/YYYY HH:MM:SS).
+     * 
      * @return array Un array asociativo de estilos para celdas de Excel.
      */
     public static function GetEstilosExcel()
@@ -59,23 +75,43 @@ class PHPSpreadsheet
         ];
     }
 
+    /**
+     * GeneraExcel
+     * 
+     * Genera un archivo Excel con los datos proporcionados.
+     *
+     * @param string $nombre_archivo Nombre del archivo Excel a generar.
+     * @param string $nombre_hoja Nombre de la hoja dentro del archivo Excel.
+     * @param string $titulo_reporte Título del reporte que se mostrará en la primera fila.
+     * @param array $columnas Arreglo de columnas con la estructura:
+     *                        [
+     *                          'letra' => 'A', // Letra de la columna
+     *                          'titulo' => 'Título de la columna', // Título de la columna
+     *                          'campo' => 'campo_de_datos', // Campo de los datos
+     *                          'estilo' => [], // Estilo de la celda
+     *                          'total' => true/false // Indica si se debe calcular el total de la columna
+     *                        ]
+     * @param array $filas Arreglo de filas con los datos a incluir en el reporte.
+     *
+     * @return void
+     */
     public static function GeneraExcel($nombre_archivo, $nombre_hoja, $titulo_reporte, $columnas, $filas)
     {
         $totales = [];
-        $spreadsheet = new Spreadsheet();
-        $sheet = $spreadsheet->getActiveSheet();
-        $sheet->setTitle($nombre_hoja);
+        $libro = new Spreadsheet();
+        $hoja = $libro->getActiveSheet();
+        $hoja->setTitle($nombre_hoja);
 
         // Título del reporte
-        $sheet->setCellValue('A1', $titulo_reporte);
-        $sheet->mergeCells('A1:' . $columnas[count($columnas) - 1]['letra'] . '1');
-        $sheet->getStyle('A1')->applyFromArray(self::GetEstilosExcel()['titulo']);
+        $hoja->setCellValue('A1', $titulo_reporte);
+        $hoja->mergeCells('A1:' . $columnas[count($columnas) - 1]['letra'] . '1');
+        $hoja->getStyle('A1')->applyFromArray(self::GetEstilosExcel()['titulo']);
 
         // Encabezados de columna
         foreach ($columnas as $key => $columna) {
-            $sheet->setCellValue($columna['letra'] . '2', $columna['titulo']);
-            $sheet->getStyle($columna['letra'] . '2')->applyFromArray(self::GetEstilosExcel()['titulo']);
-            $sheet->getColumnDimension($columna['letra'])->setAutoSize(true);
+            $hoja->setCellValue($columna['letra'] . '2', $columna['titulo']);
+            $hoja->getStyle($columna['letra'] . '2')->applyFromArray(self::GetEstilosExcel()['titulo']);
+            $hoja->getColumnDimension($columna['letra'])->setAutoSize(true);
             if ($columna['total']) array_push($totales, $columna);
         }
 
@@ -83,7 +119,7 @@ class PHPSpreadsheet
         $noFila = 3;
         foreach ($filas as $key => $fila) {
             if ($noFila % 2 == 0) {
-                $sheet->getStyle('A' . $noFila . ':' . $columnas[count($columnas) - 1]['letra'] . $noFila)
+                $hoja->getStyle('A' . $noFila . ':' . $columnas[count($columnas) - 1]['letra'] . $noFila)
                     ->getFill()
                     ->setFillType(Style\Fill::FILL_SOLID)
                     ->getStartColor()
@@ -95,15 +131,15 @@ class PHPSpreadsheet
                 $estiloCelda['borders']['left']['borderStyle'] = Style\Border::BORDER_THIN;
                 $estiloCelda['borders']['right']['borderStyle'] = Style\Border::BORDER_THIN;
 
-                $sheet->setCellValue($columna['letra'] . $noFila, html_entity_decode($fila[$columna['campo']], ENT_QUOTES, "UTF-8"));
-                $sheet->getStyle($columna['letra'] . $noFila)->applyFromArray($estiloCelda);
+                $hoja->setCellValue($columna['letra'] . $noFila, html_entity_decode($fila[$columna['campo']], ENT_QUOTES, "UTF-8"));
+                $hoja->getStyle($columna['letra'] . $noFila)->applyFromArray($estiloCelda);
             }
 
             $noFila += 1;
         }
 
         // Poner borde a la última fila
-        $sheet->getStyle('A' . ($noFila - 1) . ':' . $columnas[count($columnas) - 1]['letra'] . ($noFila - 1))
+        $hoja->getStyle('A' . ($noFila - 1) . ':' . $columnas[count($columnas) - 1]['letra'] . ($noFila - 1))
             ->applyFromArray([
                 'borders' => [
                     'bottom' => ['borderStyle' => Style\Border::BORDER_THIN]
@@ -113,33 +149,69 @@ class PHPSpreadsheet
         // Incluir totales si es necesario
         if (count($totales) > 0) {
             $noFila += 1;
-            $sheet->setCellValue('A' . $noFila, 'Totales');
-            $sheet->getStyle('A' . $noFila)->applyFromArray([
-                'font' => ['bold' => true],
-                'alignment' => ['horizontal' => Style\Alignment::HORIZONTAL_CENTER]
-            ]);
-            $sheet->getStyle('A' . $noFila . ':' . $columnas[count($columnas) - 1]['letra'] . $noFila)
-                ->applyFromArray([
-                    'borders' => [
-                        'top' => ['borderStyle' => Style\Border::BORDER_THIN],
-                        'bottom' => ['borderStyle' => Style\Border::BORDER_THIN],
-                        'left' => ['borderStyle' => Style\Border::BORDER_THIN],
-                        'right' => ['borderStyle' => Style\Border::BORDER_THIN]
-                    ],
-                    'font' => ['bold' => true]
-                ]);
-
-            foreach ($totales as $key => $total) {
-                $sheet->setCellValue($total['letra'] . $noFila, '=SUBTOTAL(9,' . $total['letra'] . '3:' . $total['letra'] . ($noFila - 1) . ')');
-                $sheet->getStyle($total['letra'] . $noFila)->applyFromArray($total['estilo']);
-            }
+            self::AddTotales($hoja, $noFila, $columnas, $totales);
         }
 
         // Seleccionar celda A1, congelar en la fila 3, aplicar filtro a las columnas
-        $sheet->setSelectedCell('A1');
-        $sheet->freezePane('A3');
-        $sheet->setAutoFilter('A2:' . $columnas[count($columnas) - 1]['letra'] . '2');
+        $hoja->setSelectedCell('A1');
+        $hoja->freezePane('A3');
+        $hoja->setAutoFilter('A2:' . $columnas[count($columnas) - 1]['letra'] . '2');
 
+        // Enviar descarga
+        self::EnviarDescarga($libro);
+    }
+
+    /**
+     * AddTotales
+     * 
+     * Agrega una fila de totales a la hoja de cálculo.
+     *
+     * @param \PhpOffice\PhpSpreadsheet\Worksheet\Worksheet $hoja La hoja de cálculo donde se agregarán los totales.
+     * @param int $noFila El número de fila donde se colocarán los totales.
+     * @param array $columnas Un arreglo de columnas que se utilizarán para aplicar estilos.
+     * @param array $totales Un arreglo de totales que contiene la letra de la columna y el estilo a aplicar.
+     *
+     * @return void
+     */
+    public function AddTotales($hoja, $noFila, $columnas, $totales)
+    {
+        $hoja->setCellValue('A' . $noFila, 'Totales');
+        $hoja->getStyle('A' . $noFila)->applyFromArray([
+            'font' => ['bold' => true],
+            'alignment' => ['horizontal' => Style\Alignment::HORIZONTAL_CENTER]
+        ]);
+
+        // Poner estilo a la fila de totales
+        $hoja->getStyle('A' . $noFila . ':' . $columnas[count($columnas) - 1]['letra'] . $noFila)
+            ->applyFromArray([
+                'borders' => [
+                    'top' => ['borderStyle' => Style\Border::BORDER_THIN],
+                    'bottom' => ['borderStyle' => Style\Border::BORDER_THIN],
+                    'left' => ['borderStyle' => Style\Border::BORDER_THIN],
+                    'right' => ['borderStyle' => Style\Border::BORDER_THIN]
+                ],
+                'font' => ['bold' => true]
+            ]);
+
+        // Poner fórmulas para totales
+        foreach ($totales as $key => $total) {
+            $hoja->setCellValue($total['letra'] . $noFila, '=SUBTOTAL(9,' . $total['letra'] . '3:' . $total['letra'] . ($noFila - 1) . ')');
+            $hoja->getStyle($total['letra'] . $noFila)->applyFromArray($total['estilo']);
+        }
+    }
+
+    /**
+     * EnviarDescarga
+     *
+     * Este método configura los encabezados HTTP necesarios para la descarga de un archivo
+     * Excel en formato .xlsx y envía el archivo al cliente.
+     *
+     * @param \PhpOffice\PhpSpreadsheet\Spreadsheet $libro El objeto Spreadsheet que se va a descargar.
+     *
+     * @return void
+     */
+    public function EnviarDescarga($libro)
+    {
         // Configuración de encabezados HTTP
         header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment;filename="' . $nombre_archivo . '.xlsx"');
@@ -148,8 +220,8 @@ class PHPSpreadsheet
         header('Last-Modified: ' . gmdate('D, d M Y H:i:s') . ' GMT');
         header('Pragma: public');
 
-        // Guardar el archivo
-        $writer = new Xlsx($spreadsheet);
+        // Mandar la descarga del archivo
+        $writer = new Xlsx($libro);
         $writer->save('php://output');
     }
 }
